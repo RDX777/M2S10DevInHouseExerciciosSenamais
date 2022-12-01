f1cc62f0f04844daaa675500e70093d25d1689d6
Anderson Tome <anderson_tome@outlook.com.br>
2022-12-01 00:57:24 -0300

    Instalação Inicial

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TransformResponseInterceptor } from './core/http/transform-response-interceptor';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+  ],
+  controllers: [],
+  providers: [{
+    provide: APP_INTERCEPTOR,
+    useClass: ClassSerializerInterceptor,
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: TransformResponseInterceptor,
+  },],
+})
+export class AppModule { }
